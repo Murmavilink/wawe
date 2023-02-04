1fb9f7ffea539cc48e6a8c27c72703dde03fe749
Mur <murmavilink@gmail.com>
2023-02-04 15:51:20 +0600

    tabs done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import { smoothScroll } from './modules/menu';
 import { addingClassScroll } from './modules/menu';
 import { burgerMenu } from './modules/menu';
 import { audio } from './modules/audio';
+import { tabs } from './modules/tabs';
+
 
 smoothScroll();
 addingClassScroll();
 burgerMenu();
 audio();
+tabs();
